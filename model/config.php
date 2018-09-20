073024c1036b5693868f383dad70c272d7cc1243
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-09-20 17:50:00 -0300

    root y password bd

--- model/config.php
+++ model/config.php
@@ -1,5 +1,5 @@
 <?php
 define('NOMBRE_SERVIDOR','localhost');
-define('NOMBRE_USUARIO','root');
-define('PASSWORD','');
+define('NOMBRE_USUARIO','grupo12');
+define('PASSWORD','MjM1ZDVlMzBhNWNl');
 define('NOMBRE_BD','grupo12');
