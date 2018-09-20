6f0b35559521634b8b7c22632ab62817e5796450
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-09-20 17:45:46 -0300

    modelo 1

---
+++ controller/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+ function abrir_conexion(){
+                include_once 'config.php';
+                try{
+                $conexion = new PDO('mysql:host='.NOMBRE_SERVIDOR.'; dbname='.NOMBRE_BD,NOMBRE_USUARIO,PASSWORD);
+                }catch(PDOException $ex){
+                    throw new Exception("error en la conexion");
+                }
+                $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conexion->exec("SET CHARACTER SET utf8");
+                return $conexion;
+
+        
+    }
---
+++ controller/usuario.php
@@ -0,0 +1,206 @@
+<?php
+class Usuario{
+  private $id;
+  private $email;
+  private $username;
+  private $password;
+  private $activo;
+  private $updated_at;
+  private $created_at;
+  private $first_name;
+  private $last_name;
+  private $borrado;
+
+  public function __construct($id,$email,$username,$password,$activo,$updated_at,$created_at,$first_name,$last_name,$borrado){
+    $this -> id = $id;
+    $this -> email = $email;
+    $this -> username = $username;
+    $this -> password = $password;
+    $this -> activo = $activo;
+    $this -> updated_at = $updated_at;
+    $this -> created_at = $created_at;
+    $this -> first_name = $first_name;
+    $this -> last_name = $last_name;
+    $this -> borrado = $borrado;
+  }
+
+  /**
+   * Get the value of id
+   */ 
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set the value of id
+   *
+   * @return  self
+   */ 
+  public function setId($id)
+  {
+    $this->id = $id;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of email
+   */ 
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  /**
+   * Set the value of email
+   *
+   * @return  self
+   */ 
+  public function setEmail($email)
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of username
+   */ 
+  public function getUsername()
+  {
+    return $this->username;
+  }
+
+  /**
+   * Set the value of username
+   *
+   * @return  self
+   */ 
+  public function setUsername($username)
+  {
+    $this->username = $username;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of password
+   */ 
+  public function getPassword()
+  {
+    return $this->password;
+  }
+
+  /**
+   * Set the value of password
+   *
+   * @return  self
+   */ 
+  public function setPassword($password)
+  {
+    $this->password = $password;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of activo
+   */ 
+  public function getActivo()
+  {
+    return $this->activo;
+  }
+
+  /**
+   * Set the value of activo
+   *
+   * @return  self
+   */ 
+  public function setActivo($activo)
+  {
+    $this->activo = $activo;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of updated_at
+   */ 
+  public function getUpdated_at()
+  {
+    return $this->updated_at;
+  }
+
+  /**
+   * Set the value of updated_at
+   *
+   * @return  self
+   */ 
+  public function setUpdated_at($updated_at)
+  {
+    $this->updated_at = $updated_at;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of created_at
+   */ 
+  public function getCreated_at()
+  {
+    return $this->created_at;
+  }
+
+  /**
+   * Set the value of created_at
+   *
+   * @return  self
+   */ 
+  public function setCreated_at($created_at)
+  {
+    $this->created_at = $created_at;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of first_name
+   */ 
+  public function getFirst_name()
+  {
+    return $this->first_name;
+  }
+
+  /**
+   * Set the value of first_name
+   *
+   * @return  self
+   */ 
+  public function setFirst_name($first_name)
+  {
+    $this->first_name = $first_name;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of last_name
+   */ 
+  public function getLast_name()
+  {
+    return $this->last_name;
+  }
+
+  /**
+   * Set the value of last_name
+   *
+   * @return  self
+   */ 
+  public function setLast_name($last_name)
+  {
+    $this->last_name = $last_name;
+
+    return $this;
+  }
+}
