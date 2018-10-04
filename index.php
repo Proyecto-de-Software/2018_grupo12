d327e24f7ca11d2516f5a044f109be3787e10b30
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-10-04 17:01:55 -0300

    prueba3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
-require_once './vendor/autoload.php';
+require_once 'autoload.php';
 
 //Agregar control de mantenimiento
 //Agregar control de sesiones
