399cb34b5c28b396b0747a7b3887b666894c368e
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-10-17 21:56:09 -0300

    correccion nn

--- model/repositorioPaciente.php
+++ model/repositorioPaciente.php
@@ -74,8 +74,8 @@ class RepositorioPaciente
          try{
             $sql = "INSERT INTO paciente(apellido,nombre,fecha_nac,lugar_nac,localidad_id,partido_id,region_sanitaria_id,
             domicilio,genero_id,tiene_documento,tipo_doc_id,numero,tel,nro_historia_clinica,nro_carpeta,obra_social_id,borrado)
-            VALUES ('NN','NN',' ',' ',null,null,null,' ',
-            null,0,null,' ',' ',:nro_historia_clinica,' ',null,0)";
+            VALUES ('NN','NN',0001-01-01,' ',null,null,null,' ',
+            null,0,null,0,' ',:nro_historia_clinica,0,null,0)";
             $sentencia=$conexion->prepare($sql);
             $sentencia->bindParam(":nro_historia_clinica",$nro_historia_clinica);
             $ok=$sentencia->execute();
