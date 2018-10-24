c1ce00e669af847471f6ab379c364128e59f8591
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-10-24 15:57:46 -0300

    cambios en repositorioRol

--- model/repositorioRol.php
+++ model/repositorioRol.php
@@ -85,4 +85,56 @@ class RepositorioRol
         $conexion = null;
         return $roles;
     }
+    public function obtener_todos_los_roles_pagina($limite,$pag)
+    { /*array vacio si no hay roles en la BD */
+        $roles = array();
+        $conexion = abrir_conexion();
+        if ($conexion !== null) {
+            try {
+                $primero = $limite * ($pag - 1);
+                $sql = "SELECT * FROM rol LIMIT :primero,:limite ";
+                $sentencia = $conexion->prepare($sql);
+                $sentencia->bindParam(":primero",$primero,PDO::PARAM_INT);
+                $sentencia->bindParam(":limite",$limite,PDO::PARAM_INT);
+                $sentencia->execute();
+                $resultado = $sentencia->fetchAll();
+                if (count($resultado)) {
+                    foreach ($resultado as $r) {
+                        $roles[] = new Rol($r['id'], $r['nombre']);
+                    }
+                }
+            } catch (PDOException $ex) {
+                throw new Exception("erro consulta repositorioRol-> obtener_todos_los_roles_pagina");
+            }
+        }
+        $conexion = null;
+        return $roles;
+    }
+    public function obtener_roles_por_nombre($nombre,$limite,$pag)
+    { /*array vacio si no hay roles en la BD */
+        $roles = array();
+        $conexion = abrir_conexion();
+        if ($conexion !== null) {
+            try {
+                $nombre= "%".$nombre."%";
+                $primero = $limite * ($pag - 1);
+                $sql = "SELECT * FROM rol WHERE nombre LIKE :nom LIMIT :primero,:limite ";
+                $sentencia = $conexion->prepare($sql);
+                $sentencia->bindParam(":nom",$nombre);
+                $sentencia->bindParam(":primero",$primero,PDO::PARAM_INT);
+                $sentencia->bindParam(":limite",$limite,PDO::PARAM_INT);
+                $sentencia->execute();
+                $resultado = $sentencia->fetchAll();
+                if (count($resultado)) {
+                    foreach ($resultado as $r) {
+                        $roles[] = new Rol($r['id'], $r['nombre']);
+                    }
+                }
+            } catch (PDOException $ex) {
+                throw new Exception("erro consulta repositorioRol-> obtener_roles_por_nombre");
+            }
+        }
+        $conexion = null;
+        return $roles;
+    }
 }
