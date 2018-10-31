fdb55d69a3c9620ebfa65e7e17622a6df21fb2de
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-10-31 15:43:14 -0300

    consulta

---
+++ model/consulta.php
@@ -0,0 +1,250 @@
+<?php
+
+class Consulta{
+    private $id;
+    private $paciente_id;
+    private $fecha;
+    private $motivo_id;
+    private $derivacion_id;
+    private $articulacion_con_instituciones;
+    private $internacion;
+    private $diagnostico;
+    private $observaciones;
+    private $tratamiento_farmacologico_id;
+    private $acompanamiento_id;
+
+    public function __construct($id,$paciente_id,$fecha,$motivo_id,$derivacion_id,$articulacion_con_instituciones,
+    $internacion,$diagnostico,$observaciones,$tratamiento_farmacologico_id,$acompanamiento_id){
+        $this -> id=$id;
+        $this-> paciente_id=$paciente_id;
+        $this -> fecha=$fecha;
+        $this -> motivo_id=$motivo_id;
+        $this -> derivacion_id=$derivacion_id;
+        $this -> articulacion_con_instituciones=$articulacion_con_instituciones;
+        $this -> internacion = $internacion;
+        $this -> diagnostico=$diagnostico;
+        $this -> observaciones =$observaciones;
+        $this -> tratamiento_farmacologico_id=$tratamiento_farmacologico_id;
+        $this -> acompanamiento_id=$acompanamiento_id;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of paciente_id
+     */ 
+    public function getPaciente_id()
+    {
+        return $this->paciente_id;
+    }
+
+    /**
+     * Set the value of paciente_id
+     *
+     * @return  self
+     */ 
+    public function setPaciente_id($paciente_id)
+    {
+        $this->paciente_id = $paciente_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fecha
+     */ 
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set the value of fecha
+     *
+     * @return  self
+     */ 
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of motivo_id
+     */ 
+    public function getMotivo_id()
+    {
+        return $this->motivo_id;
+    }
+
+    /**
+     * Set the value of motivo_id
+     *
+     * @return  self
+     */ 
+    public function setMotivo_id($motivo_id)
+    {
+        $this->motivo_id = $motivo_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of derivacion_id
+     */ 
+    public function getDerivacion_id()
+    {
+        return $this->derivacion_id;
+    }
+
+    /**
+     * Set the value of derivacion_id
+     *
+     * @return  self
+     */ 
+    public function setDerivacion_id($derivacion_id)
+    {
+        $this->derivacion_id = $derivacion_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of articulacion_con_institucion
+     */ 
+    public function getArticulacion_con_institucion()
+    {
+        return $this->articulacion_con_institucion;
+    }
+
+    /**
+     * Set the value of articulacion_con_institucion
+     *
+     * @return  self
+     */ 
+    public function setArticulacion_con_institucion($articulacion_con_instituciones)
+    {
+        $this->articulacion_con_institucion = $articulacion_con_instituciones;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of internacion
+     */ 
+    public function getInternacion()
+    {
+        return $this->internacion;
+    }
+
+    /**
+     * Set the value of internacion
+     *
+     * @return  self
+     */ 
+    public function setInternacion($internacion)
+    {
+        $this->internacion = $internacion;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of diagnostico
+     */ 
+    public function getDiagnostico()
+    {
+        return $this->diagnostico;
+    }
+
+    /**
+     * Set the value of diagnostico
+     *
+     * @return  self
+     */ 
+    public function setDiagnostico($diagnostico)
+    {
+        $this->diagnostico = $diagnostico;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of observaciones
+     */ 
+    public function getObservaciones()
+    {
+        return $this->observaciones;
+    }
+
+    /**
+     * Set the value of observaciones
+     *
+     * @return  self
+     */ 
+    public function setObservaciones($observaciones)
+    {
+        $this->observaciones = $observaciones;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of tratamiento_farmacologico_id
+     */ 
+    public function getTratamiento_farmacologico_id()
+    {
+        return $this->tratamiento_farmacologico_id;
+    }
+
+    /**
+     * Set the value of tratamiento_farmacologico_id
+     *
+     * @return  self
+     */ 
+    public function setTratamiento_farmacologico_id($tratamiento_farmacologico_id)
+    {
+        $this->tratamiento_farmacologico_id = $tratamiento_farmacologico_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of acompanamiento_id
+     */ 
+    public function getAcompanamiento_id()
+    {
+        return $this->acompanamiento_id;
+    }
+
+    /**
+     * Set the value of acompanamiento_id
+     *
+     * @return  self
+     */ 
+    public function setAcompanamiento_id($acompanamiento_id)
+    {
+        $this->acompanamiento_id = $acompanamiento_id;
+
+        return $this;
+    }
+}
---
+++ model/repositorioConsulta.php
@@ -0,0 +1,136 @@
+<?php
+
+class RepositorioConsulta
+{
+
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+    private function toNull($variable)
+    {
+        if ($variable == "" || $variable == 0) {
+            $variable = null;
+        }
+        return $variable;
+    }
+
+    public function insertar_consulta($paciente_id, $fecha, $motivo_id, $derivacion_id, $articulacion_con_instituciones,
+        $internacion, $diagnostico, $observaciones, $tratamiento_farmacologico_id, $acompanamiento_id) {
+        $ok = false;
+        $conexion = abrir_conexion();
+        if ($conexion !== null) {
+            try {
+                $sql = "INSERT INTO consulta (paciente_id,fecha,motivo_id,derivacion_id,articulacion_con_instituciones,
+                internacion,diagnostico,observaciones,tratamiento_farmacologico_id,acompanamiento_id) VALUES
+                (:paciente_id,:fecha,:motivo_id,:derivacion_id,:articulacion_con_institucion,
+                :internacion,:diagnostico,:observaciones,:tratamiento_farmacologico_id,:acompanamiento_id)";
+                $sentencia = $conexion->prepare($sql);
+                $sentencia->bindParam(":paciente_id", $paciente_id);
+                $sentencia->bindParam(":fecha", $fecha);
+                $sentencia->bindParam(":motivo_id", $motivo_id);
+                $derivacion_id = $this->toNull($derivacion_id);
+                $sentencia->bindParam(":derivacion_id", $derivacion_id);
+                $sentencia->bindParam(":articulacion_con_institucion", $articulacion_con_instituciones);
+                $sentencia->bindParam(":internacion", $internacion);
+                $sentencia->bindParam(":diagnostico", $diagnostico);
+                $sentencia->bindParam(":observaciones", $observaciones);
+                $tratamiento_farmacologico_id = $this->toNull($tratamiento_farmacologico_id);
+                $acompanamiento_id = $this->toNull($acompanamiento_id);
+                $sentencia->bindParam(":tratamiento_farmacologico_id", $tratamiento_farmacologico_id);
+                $sentencia->bindParam(":acompanamiento_id", $acompanamiento_id);
+                $ok = $sentencia->execute();
+            } catch (PDOException $ex) {
+                throw new Exception("error repositorioConsulta->insertar_consulta " . $ex->getMessage());
+            }
+        }
+        $conexion = null;
+        return $ok;
+    }
+    public function actualizar_consulta($id, $paciente_id, $fecha, $motivo_id, $derivacion_id, $articulacion_con_instituciones,
+        $internacion, $diagnostico, $observaciones, $tratamiento_farmacologico_id, $acompanamiento_id) {
+        $ok = false;
+        $conexion = abrir_conexion();
+        if ($conexion !== null) {
+            try {
+                $sql = "UPDATE consulta SET paciente_id=:paciente_id, fecha=:fecha, motivo_id=:motivo_id
+                , derivacion_id=:derivacion_id, articulacion_con_instituciones=:articulacion_con_institucion,
+                 internacion=:internacion, diagnostico=:diagnostico, observaciones=:observaciones, tratamiento_farmacologico_id=:tratamiento_farmacologico_id,
+                 acompanamiento_id=:acompanamiento_id
+                WHERE id=:id";
+                $sentencia = $conexion->prepare($sql);
+                $sentencia->bindParam(":id", $id);
+                $sentencia->bindParam(":paciente_id", $paciente_id);
+                $sentencia->bindParam(":fecha", $fecha);
+                $sentencia->bindParam(":motivo_id", $motivo_id);
+                $derivacion_id = $this->toNull($derivacion_id);
+                $sentencia->bindParam(":derivacion_id", $derivacion_id);
+                $sentencia->bindParam(":articulacion_con_institucion", $articulacion_con_instituciones);
+                $sentencia->bindParam(":internacion", $internacion);
+                $sentencia->bindParam(":diagnostico", $diagnostico);
+                $sentencia->bindParam(":observaciones", $observaciones);
+                $tratamiento_farmacologico_id = $this->toNull($tratamiento_farmacologico_id);
+                $acompanamiento_id = $this->toNull($acompanamiento_id);
+                $sentencia->bindParam(":tratamiento_farmacologico_id", $tratamiento_farmacologico_id);
+                $sentencia->bindParam(":acompanamiento_id", $acompanamiento_id);
+                $ok = $sentencia->execute();
+            } catch (PDOException $ex) {
+                throw new Exception("error repositorioConsulta->actualizar_consulta " . $ex->getMessage());
+            }
+
+        }
+    }
+    public function obtener_por_id($id)
+    {
+        $consulta = null;
+        $conexion = abrir_conexion();
+        if ($conexion !== null) {
+            try{
+                $sql="SELECT * FROM consulta WHERE id=:id";
+                $sentencia=$conexion->prepare($sql);
+                $sentencia->bindParam(":id",$id);
+                $sentencia->execute();
+                $re=$sentencia->fetch();
+                if(!empty($re)){
+                    $consulta=new Consulta($re['id'],$re['paciente_id'],$re['fecha'],$re['motivo_id'],$re['derivacion_id'],
+                    $re['articulacion_con_instituciones'],$re['internacion'],$re['diagnostico'],$re['observaciones'],$re['tratamiento_farmacologico_id'],
+                    $re['acompanamiento_id']);
+                }
+            }catch(PDOException $ex){
+                throw new Exception("erro consulta repositorioConsutla->obtener_por_id ".$ex->getMessage());
+            }
+
+        }
+        $conexion=null;
+        return $consulta;
+    }
+    public function obtener_info_completa($id){
+        $resultado = null;
+        $conexion = abrir_conexion();
+        if ($conexion !== null) {
+            try{
+                $sql="SELECT Distinct c.id,c.paciente_id,c.fecha,m.nombre as motivo,i.nombre as institucion,c.articulacion_con_instituciones,c.internacion,
+                c.diagnostico,c.observaciones,tf.nombre as tratamiento_farmacologico,a.nombre as acompanamiento
+                FROM consulta c LEFT JOIN motivo_consulta m ON(c.motivo_id=m.id)
+                                LEFT JOIN institucion i ON (c.derivacion_id=i.id)
+                                LEFT JOIN tratamiento_farmacologico tf ON(c.tratamiento_farmacologico_id=tf.id)
+                                LEFT JOIN acompanamiento a ON(c.acompanamiento_id=a.id)
+                WHERE c.id=:id";
+                $sentencia=$conexion->prepare($sql);
+                $sentencia->bindParam(":id",$id);
+                $sentencia->execute();
+                $resultado=$sentencia->fetch(PDO::FETCH_ASSOC);
+            }catch(PDOExeption $ex){
+                throw new Exception ("error obtener_info_completa ".$ex->getMessage());
+            }
+    }
+    $conexion=null;
+    return $resultado;
+}
+}
