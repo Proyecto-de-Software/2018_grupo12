4af00989c9e9e1cce749e85754dc17fbdd1ccc70
Prince, Diego Abel <diegoprince08@hotmail.com>
2019-05-15 17:04:25 -0300

    correccion display pestaña instituciones

--- Final/resources/views/moduloInstitucion/tab_new.blade.php
+++ Final/resources/views/moduloInstitucion/tab_new.blade.php
@@ -1,3 +1,3 @@
 <li id="tabAgregarInstitucion" class="nav-item">
-    <a class="nav-link" data-toggle="tab" href="#contenidoAgregarInstitucion" role="tab" aria-controls="contenidoAgregarInstitucion" aria-selected="false">Agregar Institución</a>
+    <a class="nav-link" data-toggle="tab" href="#contenidoAgregarInstitucion" role="tab" aria-controls="contenidoAgregarInstitucion" aria-selected="false" v-on:click="ocultarPestana">Agregar Institución</a>
 </li>
